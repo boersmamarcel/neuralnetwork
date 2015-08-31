6c97b0cb0a54dbee7495fef6a1e297c40b567aa7
Marcel Boersma <boersma.marcel@gmail.com>
2015-08-31 16:56:58 +0200

    Adds angle computation to dataset generation and loads the inseparable data points file

--- assignment1/assignment1.m
+++ assignment1/assignment1.m
@@ -1,14 +1,40 @@
-[a,b,class1]=generateData(500,1,2,2.1,1,0.2)
-[c,d,class2]=generateData(500,1,2,1.9,-1,0.2)
-w0=ones(1,500)
-A=[w0;a;b;class1]
-B=[w0;c;d;class2]
-M=[A B]
-M=M.'
-input=M(:,1:3)
-output=M(:,4)
-weights=perceptronConvergenceAlgorithm(input, output, [0 0 0], 0.1, 100)
+degree = 60; %set rotation degree
+[a,b,class1]=generateData(500,2,0,0,1,0.1, degree); %generate class 1 data
+[c,d,class2]=generateData(500,2,0,0,-1,0.1, degree); %generate class 2 data
 
-alpha=-weights(1)/weights(3)
-beta=-weights(2)/weights(3)
-feedbackPlot(M(:,2),M(:,3),M(:,4),alpha,beta)
+w0=ones(1,500); %generate bias term
+A=[w0;a;b;class1]; %add bias term to class 1
+B=[w0;c;d;class2]; %add bias term to class 2
+M=[A B]; %merge data to one frame
+M=M.'; %transpose the frame
+input=M(:,1:3); %input for the PCA 
+output=M(:,4); %output for the PCA 
+
+%get weights from the PCA 
+weights=perceptronConvergenceAlgorithm(input, output, [0 0 0], 0.05, 1000);
+
+%calculate line coeficients from the weights
+alpha=-weights(1)/weights(3);
+beta=-weights(2)/weights(3);
+
+%plot the results
+feedbackPlot(M(:,2),M(:,3),M(:,4),alpha,beta);
+
+data = importdata('two_class_example_not_separable.dat');
+
+[row,col] = size(data);
+
+bias = ones(row, 1); %generate bias term
+input = [bias, data(:,1:2)];
+output = data(:,3);
+
+weights=perceptronConvergenceAlgorithm(input, output, [0 0 0], 0.05, 1000);
+
+%calculate line coeficients from the weights
+alpha=-weights(1)/weights(3);
+beta=-weights(2)/weights(3);
+
+%plot the results
+feedbackPlot(data(:,1),data(:,2),data(:,3),alpha,beta);
+
+
