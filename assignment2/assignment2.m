69d067284864cb129b0bf8ac6be9ac23876e55ca
Marcel Boersma <boersma.marcel@gmail.com>
2015-09-07 14:11:58 +0200

    Whoops, the weights were correct. Undo the change

--- assignment2/assignment2.m
+++ assignment2/assignment2.m
@@ -58,7 +58,7 @@ for i = 1:12
     t1 = px_c1*prior_c1;
     t2 = px_c2*prior_c2;
     
-    p_error = sum(t1((i+1):12))*2 + sum(t2(1:i));
+    p_error = sum(t1((i+1):12)) + sum(t2(1:i))*2;
     
     errorsWeighted = [errorsWeighted p_error];
 end
