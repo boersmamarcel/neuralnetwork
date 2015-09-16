5a85188a07a8c252ca325eb00e9be74c4a56af81
elsweinans <elsweinans@gmail.com>
2015-09-16 14:10:51 +0200

    error plots for 3a

--- assignment3/a3pA.m
+++ assignment3/a3pA.m
@@ -4,34 +4,46 @@ for k = 1:length(files)
     data = importdata(files{k}, '-mat');
     
     %plot the data
-    figure;
-    scatter(data.('x'), data.('t'));
+    %figure;
+    %scatter(data.('x'), data.('t'));
 
     hidden_layers = [2,3,4,5,7,10,15,25];
+    
+    errors=[];
 
     for i = 1:length(hidden_layers) 
         hidden_layer = hidden_layers(i);
+        
 
         %5-fold cross validation
         fold = 5;
         indices = crossvalind('Kfold', length(data.('x')), fold); %randomly assigns indices
+        
 
         for j = 1:fold
             testIdx = (indices == j); 
             trainIdx = ~testIdx;
 
              %design network, one input layer, one output layer various hidden layers
-            [train, error, net] = NeuralNetworkLine(data.('x')(trainIdx), data.('t')(trainIdx), i, 100);
+            [train, error, net] = NeuralNetworkLine(data.('x')(trainIdx), data.('t')(trainIdx), hidden_layer, 1500);
 
             y = mlpfwd(train, data.('x')(testIdx));
+            
+            error=(rms(data.('t')(testIdx)-y)); 
+            errors=[errors error];
 
         end
+        
+        
 
         figure;
         scatter(data.('x'), data.('t')); hold on;
         scatter(data.('x')(testIdx), y, 'x');
     end
 end
+
+
+
 
 
 
