0d1a0cb4ffb9d33cd4de33eb50251571610cdad2
Marcel Boersma <boersma.marcel@gmail.com>
2015-09-19 13:59:30 +0200

    General setup assignment 4

--- assignment4/a4pB.m
+++ assignment4/a4pB.m
@@ -2,16 +2,34 @@
 
 data = importdata('owndata.mat', '-mat');
 
-% [yp,alpha,b,gam,sig2,model] = lssvm([data(:,2),data(:,3)],data(:,4),'c', 'poly_kernel');
 
 X = data(:,2:3);
 Y = data(:, 4);
-gam = 10;
-sig2 = 0.2;
+
 type='c'; %classification
 kernel = 'RBF_kernel'; %RBF_kernel/lin_kernel/poly_kernel
 dataprocessing = 'preprocess'; % preprocess/original
-[alpha,b] = trainlssvm({X,Y,type,gam,sig2,kernel,dataprocessing});
+%GAM: regularization parameter
+% for gam low minimizing of the
+% complexity of the model is emphasized, for gam high, good fitting
+% of the training data points is stressed.
+gam = 1;
 
-Ytest = simlssvm({X,Y,type,gam,sig2,kernel,dataprocessing},{alpha,b},X);
-figure; plotlssvm({X,Y,type,gam,sig2,kernel,dataprocessing},{alpha,b});
+if strcmp(kernel, 'poly_kernel')
+    degree = 10;
+    highlowbalance = 0.5;
+    model = {X,Y,type,gam,[highlowbalance degree],kernel};
+   
+elseif strcmp(kernel, 'RBF_kernel')
+    sig2 = 0.2;
+    model = {X,Y,type,gam,sig2,kernel,dataprocessing};
+        
+else
+    model = {X,Y,type,gam,[],kernel,dataprocessing};
+    
+end
+
+[alpha,b] = trainlssvm(model);
+Ytest = simlssvm(model, {alpha, b}, X);
+figure; plotlssvm(model, {alpha, b});
+
--- assignment4/a4pC.m
+++ assignment4/a4pC.m
@@ -2,12 +2,32 @@ data = importdata('pics.mat', '-mat');
 
 X = data.pics;
 Y = data.classGlass.';
-gam = 10;
-sig2 = 0.2;
+
+
 type='c'; %classification
 kernel = 'RBF_kernel'; %RBF_kernel/lin_kernel/poly_kernel
 dataprocessing = 'preprocess'; % preprocess/original
-[alpha,b] = trainlssvm({X,Y,type,gam,sig2,kernel,dataprocessing});
+%GAM: regularization parameter
+% for gam low minimizing of the
+% complexity of the model is emphasized, for gam high, good fitting
+% of the training data points is stressed.
+gam = 1;
 
-Ytest = simlssvm({X,Y,type,gam,sig2,kernel,dataprocessing},{alpha,b},X);
-figure; plotlssvm({X,Y,type,gam,sig2,kernel,dataprocessing},{alpha,b});
+if strcmp(kernel, 'poly_kernel')
+    degree = 10;
+    highlowbalance = 0.5;
+    model = {X,Y,type,gam,[highlowbalance degree],kernel};
+   
+elseif strcmp(kernel, 'RBF_kernel')
+    sig2 = 0.2;
+    model = {X,Y,type,gam,sig2,kernel,dataprocessing};
+        
+else
+    model = {X,Y,type,gam,[],kernel,dataprocessing};
+    
+end
+
+[alpha,b] = trainlssvm(model);
+Ytest = simlssvm(model, {alpha, b}, X);
+figure; plotlssvm(model, {alpha, b});
+
