1e44d1f76593034f61d7e523489614177c51db44
elsweinans <elsweinans@gmail.com>
2015-09-21 17:16:12 +0200

    blaat

--- assignment3/a3pA_polyfit.m
+++ assignment3/a3pA_polyfit.m
@@ -1,4 +1,5 @@
-files = {'line.mat'; 'sinus.mat'; 'irregular.mat'};
+%files = {'line.mat'; 'sinus.mat'; 'irregular.mat'};
+files={'line.mat'; 'sinus.mat'}
 
 for k = 1:length(files)
     data = importdata(files{k}, '-mat');
@@ -6,56 +7,75 @@ for k = 1:length(files)
     input=data.('x');
     output=data.('t');
     
-    degrees=[1,2,3,4,5,6,7];
-    errors_test=[];
-    errors_train=[];
+    degrees=[1,2,3,4,5,6];
     
+  
+    
+    RMStest=[];
+    RMStrain=[];
+ 
+        
     for i = 1:length(degrees);
         degree=degrees(i);
         
+        final_errors_test=[];
+        final_errors_train=[];
+        
+        for l = 1:100;
                 %5-fold cross validation
         fold = 5;
         indices = crossvalind('Kfold', length(data.('x')), fold); %randomly assigns indices
+        errors_test=[];
+        errors_train=[];
         for j=1:fold
             testIdx = (indices == j); 
             trainIdx = ~testIdx;
-            newdata = (data.('x')-mean(data.('x')))/std(data.('x'));
+            %newdata = (data.('x')-mean(data.('x')))/std(data.('x'));
+            
+            [p]=polyfit(data.('x')(trainIdx),data.('t')(trainIdx),degree);
+            x1=linspace(min(data.('x')),max(data.('x'))); 
     
-            [p,S,mu]=polyfit(newdata(trainIdx),data.('t')(trainIdx),degree);
-            x1=linspace(min(newdata),max(newdata));  
+            %[p,S,mu]=polyfit(data.('x')(trainIdx),data.('t')(trainIdx),degree);
+            %x1=linspace(min(newdata),max(newdata));  
             y=polyval(p,x1);
         
-            error_test=(rms(data.('t')(testIdx)-y(testIdx).')); 
+            error_test=sqrt((1/5)*(sum((polyval(p,data.x(testIdx))-data.('t')(testIdx)).^2))); 
             errors_test=[errors_test error_test];
             
-            error_train=(rms(data.('t')(trainIdx)-y(trainIdx).')); 
+            error_train=sqrt((1/20)*(sum((polyval(p,data.x(trainIdx))-data.('t')(trainIdx)).^2))); 
             errors_train=[errors_train error_train];
+            
+           
+            
 
         end
         
-    figure;    
-    plot(x1,y); hold on;
+         mean_errors_test=mean(errors_test);
+         mean_errors_train=mean(errors_train);
+    %figure;    
+    %plot(x1,y); hold on;
     
-    scatter(newdata, data.t, '*');
-    
+    %scatter(newdata, data.t, '*');
+    final_errors_test=[final_errors_test mean_errors_test];
+    final_errors_train=[final_errors_train mean_errors_train];
 
-    end
+        end    
+       
+    RMStest=[RMStest mean(final_errors_test)];
+    RMStrain=[RMStrain mean(final_errors_train)];
     
-    mean_errors_test=[];
-    mean_errors_train=[];
-    for i = 1:length(degrees);
-        mean_error_test=mean(errors_test((i-1)*fold+1:i*fold));
-        mean_errors_test=[mean_errors_test mean_error_test];
-        mean_error_train=mean(errors_train((i-1)*fold+1:i*fold));
-        mean_errors_train=[mean_errors_train mean_error_train];
+      
+  
+    
+    
     end
     
     figure;
-    plot(degrees,mean_errors_test); hold on;
-    plot(degrees,mean_errors_train);
+    plot(degrees,RMStest); hold on;
+    plot(degrees,RMStrain);
     xlabel('degrees of the polynomial')
-    ylabel('average error')
+    ylabel('RMS')
     legend('error test set','error training set')
-    
-    
+
 end
+
--- assignment3/a3pB.m
+++ assignment3/a3pB.m
@@ -11,15 +11,23 @@ dataNoGlass = data.pics(noGlassIdx,:); % no glass data
 %construct rbf network
 input_nodes = 56*46;
 output_nodes = 1;
-hidden_nodes = [10, 50, 100, 200, 500, 1000];
+hidden_nodes = [100,200,500,1000,1500];
 
-errors = [];
+RMStest=[];
+RMStrain=[];
 
 for i = 1:length(hidden_nodes)
+    final_errors_test=[];
+    final_errors_train=[];
     fprintf('Neural network with hidden nodes:%d\n', hidden_nodes(i));
+    
+    for l=1:10
     fold = 10;
     indicesGlass = crossvalind('Kfold', sum(glassIdx), fold);
     indicesNoGlass = crossvalind('Kfold', length(data.class) - sum(glassIdx), fold);
+    
+    errors_test=[];
+    errors_train=[];
     
     for j = 1:fold
 
@@ -35,29 +43,37 @@ for i = 1:length(hidden_nodes)
             trainClass = [ones(length(dataGlass(trainIdxGlass)),1); zeros(length(dataNoGlass(trainIdxNoGlass)),1)];
             
             
-            net = mlp(input_nodes, hidden_nodes(i), output_nodes, 'linear');
+            net = mlp(input_nodes, hidden_nodes(i), output_nodes, 'logistic');
             [trainNet, errorMLP] = mlptrain(net, trainData, trainClass, 40);
 
              y = mlpfwd(trainNet, testData);
              y = y>0;
+             ytrain=mlpfwd(trainNet,trainData);
+             ytrain=ytrain>0;
 
-             error=(rms(testClass-y));
+             error_test=(rms(testClass-y));
+             error_train=rms(trainClass-ytrain);
 
-             errors = [errors error];
+             errors_test = [errors_test error_test];
+             errors_train = [errors_train error_train];
             
     end
     
-end
-
-mean_errors=[];
-for i = 1:length(hidden_nodes);
-    mean_error=mean(errors((i-1)*10+1:i*10));
-    mean_errors=[mean_errors mean_error];
+    final_errors_test=[final_errors_test mean(errors_test)];
+    final_errors_train=[final_errors_train mean(errors_train)];
+    
+    end
+    
+    RMStest=[RMStest mean(final_errors_test)];
+    RMStrain=[RMStrain mean(final_errors_train)];
+    
+    
 end
 
 figure;
-plot(hidden_nodes,mean_errors);
-xlabel('number of hidden layers')
-ylabel('average error')
-ylim([0,1])
+    plot(hidden_nodes,RMStest); hold on;
+    plot(hidden_nodes,RMStrain);
+    xlabel('number of hidden layers')
+    ylabel('RMS')
+    legend('error test set','error training set')
 
