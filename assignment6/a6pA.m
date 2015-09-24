5ee3243e1f8c6d5a9cd79ab731a437989a286577
elsweinans <elsweinans@gmail.com>
2015-09-24 14:04:35 +0200

    assignment 6

--- assignment6/a6pA.m
+++ assignment6/a6pA.m
@@ -1,24 +1,32 @@
 X = exprnd(2,[1,500]);
+X=1-(X/max(X));
 Y = randn(500,1);
-scatter(16-X,Y);
+Y=Y/max(Y)
+figure;
+scatter(X,Y,'bo'); hold on; 
+
 M=[X.' Y];
 
-%set parameters
-epochs=100; 
-initial_learning_rate=0.9;
-final_learning_rate=0.05;
+net=som(2,[50,50]);
 
-net=som(500,[10,10]);
+options = foptions;
+options(14) = 1000  %epochs ;
+options(18) = 0.9   %initial_learning_rate;  
+options(16) = 0.05  %final_learning_rate; 
+options(17) = 8;    %Initial neighbourhood size
+options(15) = 1;    %Final neighbourhood size
 
-options(14)=epochs ;
-options(18) = initial_learning_rate;  
-options(16) = final_learning_rate; 
+net2=somtrain(net,options,M);
+c2 = sompak(net2);
+figure;
+plot(c2(:, 1), c2(:, 2), 'r*'); hold on;
 
-net2=somtrain(net,options,M.');
 
-options(14) = 400;
-options(18) = 0.05;
-options(16) = 0.01;
-options(17) = 0;
-options(15) = 0;
-net3 = somtrain(net2, options, M.');
+options(14) = 400;  %epochs
+options(18) = 0.01; %initial_learning_rate; 
+options(16) = 0.01; %final_learning_rate;
+options(17) = 1;    %Initial neighbourhood size
+options(15) = 0;    %Final neighbourhood size
+net3 = somtrain(net2, options, M);
+c3 = sompak(net3);
+plot(c3(:, 1), c3(:, 2), 'g^');
